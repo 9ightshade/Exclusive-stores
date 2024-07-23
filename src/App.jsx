f6e4aed4b713fdf3b49ab8b8990f71d48b050b3b
9ightshade <aniekemeusanga@gmail.com>
2024-07-23 11:50:33 +0100

    chore:scaffolding project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <p className='text-blue' >
+        Exclusive stores
+      </p>
+    </div>
+  )
+}
+
+export default App
